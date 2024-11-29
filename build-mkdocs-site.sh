f3949ea689d45c699ff6f4940eed1c67adebeb12
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-11-29 11:35:10 +0000

    A script to build and serve a mkdocs site

---
+++ build-mkdocs-site.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# Install and update packages
+
+dnf update -y
+dnf install -y vim git python3-pip # httpd
+
+# Set up mkdocs
+
+python -m venv mkdocs
+source /root/mkdocs/bin/activate
+pip install -y mkdocs
+
+# mkdocs plugins, if any, need to be installed here
+
+git clone https://github.com/UCL-ARC/mkdocs-demo.git
+cd mkdocs-demo
+mkdocs build
+
+# Serve the site
+
+## Replace this with httpd or whatever web server solution
+## static site files are at /root/<repo name>/site
+mkdocs serve
