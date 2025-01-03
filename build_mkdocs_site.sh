4a7ee4c173330e107acc74a05be8da29d75d3f24
Camilla Harris <camilla.harris@ucl.ac.uk>
2025-01-03 14:52:00 +0000

    use material, enable httpd, and parameterize repo branch

--- build_mkdocs_site.sh
+++ build_mkdocs_site.sh
@@ -10,15 +10,19 @@ dnf install -y vim git python3-pip httpd
 python -m venv mkdocs
 source /root/mkdocs/bin/activate
 python -m pip install --upgrade pip
-pip install mkdocs
+pip install mkdocs-material
 
-# mkdocs plugins, if any, need to be installed here
+# plugins, if any, need to be installed here
 
-git clone --depth 1 ${mkdocs_repo_url} mkdocs-source
+pip install mkdocs-git-revision-date-localized-plugin
+
+# Build the site
+
+git clone --depth 1 ${mkdocs_repo_url} -b ${mkdocs_repo_branch} mkdocs-source
 cd mkdocs-source
 mkdocs build
 
 # Serve the site
 
 cp -r site/* /var/www/html/
-systemctl start httpd
+systemctl enable --now httpd
