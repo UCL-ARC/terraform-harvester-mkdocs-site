3715facd0d95c0d184a5c6e74c3a18cf8950b0e4
Camilla Harris <camilla.harris@ucl.ac.uk>
2025-01-16 09:42:17 +0000

    serve the site on https

--- build_mkdocs_site.sh
+++ build_mkdocs_site.sh
@@ -20,5 +20,6 @@ mkdocs build
 
 # Serve the site
 
-cp -r site/* /var/www/html/
+sed -i 's/Listen 80/#Listen 80/g' /etc/httpd/conf/httpd.conf
+cp -r site/* /var/www/https/
 systemctl enable --now httpd
